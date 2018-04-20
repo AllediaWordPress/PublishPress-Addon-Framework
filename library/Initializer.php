f3d4e7cd6f2c8c2edf8b38c4bede4907d51c7853
Anderson Grudtner Martins <anderson@ostraining.com>
2018-04-19 22:48:04 -0300

    Add basic library

---
+++ library/Initializer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Add-on initializer class.
+ *
+ * @package     PublishPressAddonFramework
+ * @author      PublishPress <help@publishpress.com>
+ * @copyright   Copyright (C) 2018 PublishPress. All rights reserved.
+ * @license     GPLv2 or later
+ * @since       1.0.0
+ */
+
+class Initializer
+{
+
+}
